66cb5ee7289e4daa267ad03bae62b734b87907e8
Edward Chan <esh@mini-esh.(none)>
2011-01-01 18:43:12 +0900

    simplify http-post and http-get

--- src/intrade_tradeservice/core.clj
+++ src/intrade_tradeservice/core.clj
@@ -6,16 +6,14 @@
 
 (def *user-agent* "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.7) Gecko/20100106 Ubuntu/9.10 (karmic) Firefox/3.5.7")
 
-(defn http-post [protocol site path cookies params body]
+(defn http-post [url cookies params body]
 	(let [client (new HttpClient)
-	      method (new PostMethod path)
-	      init-state (new HttpState)
-	      port (if (= protocol "https") 443 80)]
+	      method (new PostMethod url)
+	      init-state (new HttpState)]
 		(try
 			(dorun (map #(.addCookie init-state %) cookies))
 			(dorun (map #(.setParameter (.getParams client) (key %) (val %)) (seq params)))
 			(.setCookiePolicy (.getParams client) CookiePolicy/BROWSER_COMPATIBILITY)
-			(.setHost (.getHostConfiguration client) site port protocol)
 			(.setState client init-state)
 			(.setRequestBody method (into-array NameValuePair (map #(new NameValuePair (key %) (val %)) (seq body))))
 
@@ -28,16 +26,14 @@
 
 			(finally (.releaseConnection method)))))
 
-(defn http-get [protocol site path cookies]
+(defn http-get [url cookies]
 	(let [client (new HttpClient)
-	      method (new GetMethod path)
-	      init-state (new HttpState)
-	      port (if (= protocol "https") 443 80)]
+	      method (new GetMethod url)
+	      init-state (new HttpState)]
 		(try
 			(dorun (map #(.addCookie init-state %) cookies))
 			(.setParameter (.getParams client) HttpMethodParams/USER_AGENT *user-agent*)
 			(.setCookiePolicy (.getParams client) CookiePolicy/BROWSER_COMPATIBILITY)
-			(.setHost (.getHostConfiguration client) site 80 "http")
 			(.setState client init-state)
 
 			{:status (.executeMethod client method)
@@ -45,8 +41,8 @@
 			 :body (new String (.getResponseBody method))}
 			(finally (.releaseConnection method)))))
 
-(defn login [site username password]
-	(let [login1 (http-post "https" site "/" ()
+(defn login [url username password]
+	(let [login1 (http-post url ()
 		   {HttpMethodParams/USER_AGENT *user-agent*
 		    "Referer" "http://play.intrade.com/jsp/intrade/contractSearch/"}
 		   {"request_operation" "login"
