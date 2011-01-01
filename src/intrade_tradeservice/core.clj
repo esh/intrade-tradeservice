72863a791d6d5029c8eebe3bbf800c65f25840ac
Edward Chan <esh@mini-esh.(none)>
2011-01-01 19:00:19 +0900

    it logged in..

--- src/intrade_tradeservice/core.clj
+++ src/intrade_tradeservice/core.clj
@@ -6,16 +6,15 @@
 
 (def *user-agent* "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.7) Gecko/20100106 Ubuntu/9.10 (karmic) Firefox/3.5.7")
 
-(defn http-post [url cookies params body]
+(defn http-req [method cookies params body]
 	(let [client (new HttpClient)
-	      method (new PostMethod url)
 	      init-state (new HttpState)]
 		(try
 			(dorun (map #(.addCookie init-state %) cookies))
 			(dorun (map #(.setParameter (.getParams client) (key %) (val %)) (seq params)))
 			(.setCookiePolicy (.getParams client) CookiePolicy/BROWSER_COMPATIBILITY)
 			(.setState client init-state)
-			(.setRequestBody method (into-array NameValuePair (map #(new NameValuePair (key %) (val %)) (seq body))))
+			(if (not (= nil body)) (.setRequestBody method (into-array NameValuePair (map #(new NameValuePair (key %) (val %)) (seq body)))))
 
 			(let [status (.executeMethod client method)
 			      cookies (seq (.getCookies (.getState client)))
@@ -26,33 +25,29 @@
 
 			(finally (.releaseConnection method)))))
 
-(defn http-get [url cookies]
-	(let [client (new HttpClient)
-	      method (new GetMethod url)
-	      init-state (new HttpState)]
-		(try
-			(dorun (map #(.addCookie init-state %) cookies))
-			(.setParameter (.getParams client) HttpMethodParams/USER_AGENT *user-agent*)
-			(.setCookiePolicy (.getParams client) CookiePolicy/BROWSER_COMPATIBILITY)
-			(.setState client init-state)
+(defn http-post [url cookies params body] (http-req (new PostMethod url) cookies params body))
 
-			{:status (.executeMethod client method)
-			 :cookies (seq (.getCookies (.getState client)))
-			 :body (new String (.getResponseBody method))}
-			(finally (.releaseConnection method)))))
+
+(defn http-get [url cookies params] (http-req (new GetMethod url) cookies params nil))
 
 (defn login [url username password]
-	(let [login1 (http-post url ()
-		   {HttpMethodParams/USER_AGENT *user-agent*
-		    "Referer" "http://play.intrade.com/jsp/intrade/contractSearch/"}
-		   {"request_operation" "login"
-		    "request_type" "action"
-		    "contractBook" "none"
-		    "forwardpage" "intersiteLogin"
-		    "membershipNumber" username
-		    "password" password})
-	      ]
-		login1))
+	(let [login1 (http-post
+		"https://www.intrade.com"	
+		()
+		{HttpMethodParams/USER_AGENT *user-agent*
+		"Referer" url}
+		{"request_operation" "login"
+		"request_type" "action"
+		"contractBook" "none"
+		"forwardpage" "intersiteLogin"
+		"membershipNumber" username
+		"password" password})
+	      login2 (http-get
+		(get login1 :location)
+		(get login1 :cookies)
+		{HttpMethodParams/USER_AGENT *user-agent*
+		"Referer" url})]
+		login2))
 
 (defn logout)
 
